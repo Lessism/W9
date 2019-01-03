4f1bd13889707a7a5627a61bc5ea1438015ed3ab
Less <46290638+Lessism@users.noreply.github.com>
2019-01-03 23:29:48 +0900

    MainC

---
+++ Controller/MainC.java
@@ -0,0 +1,28 @@
+package com.lessism.w9;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainC {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MainC.class);
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+		public String home(Locale locale, Model model) {
+			logger.info("Welcome home! The client locale is {}.", locale);
+			Date date = new Date();
+			DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+			String formattedDate = dateFormat.format(date);
+			model.addAttribute("serverTime", formattedDate );
+			return "main";
+		}
+	
+}
