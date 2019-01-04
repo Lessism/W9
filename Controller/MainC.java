c7eba602e990f8a9cca5415e92216981720a8e93
Less <46290638+Lessism@users.noreply.github.com>
2019-01-04 23:59:55 +0900

    Update MainC.java

--- Controller/MainC.java
+++ Controller/MainC.java
@@ -1,4 +1,4 @@
-package com.lessism.w9;
+package com.ds.sts;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -7,6 +7,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
