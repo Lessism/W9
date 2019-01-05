a9527ffe167c37c734d653ba46ff986d1e67256c
Less <46290638+Lessism@users.noreply.github.com>
2019-01-05 20:23:53 +0900

    Update MemberC.java
    form ModelandView insert db

--- Controller/MemberC.java
+++ Controller/MemberC.java
@@ -1,15 +1,29 @@
 package com.lessism.w9;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.lessism.w9.dao.MemberDAO;
+import com.lessism.w9.vo.MemberVO;
 
 @Controller
 @RequestMapping("/member")
 public class MemberC {
 	
+	@Autowired
+	private MemberDAO mdao = null;
+	
 	@RequestMapping(value="/join.w9", method=RequestMethod.GET)
-	public String Join() {
-		return "member/join";
+	public ModelAndView Join() {
+		return new ModelAndView("member/join", "mvo", new MemberVO());
+	}
+	@RequestMapping(value="/join.w9", method=RequestMethod.POST)
+	public String Join(@ModelAttribute("mvo") MemberVO mvo) {
+		mdao.Join(mvo);
+		return "redirect:/";
 	}
 }
