501a788378ecd12cc0bd5b187c13bcb369740fdd
Less <46290638+Lessism@users.noreply.github.com>
2019-01-05 20:27:17 +0900

    Create MemberVO.java
    form ModelandView insert db

---
+++ VO/MemberVO.java
@@ -0,0 +1,34 @@
+package com.lessism.w9.vo;
+
+public class MemberVO {
+	
+	private String id = null;
+	private String pw = null;
+	private String nick = null;
+	private String email = null;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getNick() {
+		return nick;
+	}
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
