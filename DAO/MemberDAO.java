00c65846c218c6548199fedbffbb831c7e0f2cf3
Less <46290638+Lessism@users.noreply.github.com>
2019-01-05 20:25:34 +0900

    Create MemberDAO.java
    form ModelandView insert db

---
+++ DAO/MemberDAO.java
@@ -0,0 +1,21 @@
+package com.lessism.w9.dao;
+
+import javax.annotation.Resource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lessism.w9.vo.MemberVO;
+
+@Service
+public class MemberDAO {
+	
+	@Autowired
+	@Resource(name="sqlSession")
+	private SqlSession db = null;
+	
+	public int Join(MemberVO mvo) {
+		return db.insert("Member.join", mvo);
+	}
+}
