3fd4c5b22948884c79aab984e55fc46de8bf0b19
Less <46290638+Lessism@users.noreply.github.com>
2019-01-06 23:28:27 +0900

    Create BoardVO.java
    Create Board

---
+++ VO/BoardVO.java
@@ -0,0 +1,49 @@
+package com.lessism.w9.vo;
+
+public class BoardVO {
+	
+	private int no = 0;
+	private String writer = null;
+	private String title = null;
+	private String content = null;
+	private int hit = 0;
+	private String writedate = null;
+	
+	
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getHit() {
+		return hit;
+	}
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+	public String getWritedate() {
+		return writedate;
+	}
+	public void setWritedate(String writedate) {
+		this.writedate = writedate;
+	}
+}
