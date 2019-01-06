de268b9295908b75386f715e859b33f3ba8ffceb
Less <46290638+Lessism@users.noreply.github.com>
2019-01-06 11:33:02 +0900

    Update MemberC.java

--- Controller/MemberC.java
+++ Controller/MemberC.java
@@ -1,5 +1,7 @@
 package com.lessism.w9;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -21,9 +23,28 @@ public class MemberC {
 	public ModelAndView Join() {
 		return new ModelAndView("member/join", "mvo", new MemberVO());
 	}
+	
 	@RequestMapping(value="/join.w9", method=RequestMethod.POST)
 	public String Join(@ModelAttribute("mvo") MemberVO mvo) {
 		mdao.Join(mvo);
 		return "redirect:/";
 	}
+	
+	@RequestMapping(value="/login.w9", method=RequestMethod.GET)
+	public ModelAndView Login() {
+		return new ModelAndView("member/login", "mvo", new MemberVO());
+	}
+	
+	@RequestMapping(value="/login.w9", method=RequestMethod.POST)
+	public String Login(@ModelAttribute("mvo") MemberVO mvo, HttpSession session) {
+		MemberVO rmvo = mdao.Login(mvo);
+		if (rmvo != null) {
+			session.setAttribute("login", true);
+			session.setAttribute("nick", rmvo.getNick());
+			return "redirect:/";
+		} else {
+			return "redirect:/member/login.w9";
+		}
+	}
+	
 }
