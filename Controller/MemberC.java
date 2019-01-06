0f9f95e22b2e4dab5b43acc1f291e52aefb8bafd
Less <46290638+Lessism@users.noreply.github.com>
2019-01-06 14:22:09 +0900

    Update MemberC.java
    ver.Member

--- Controller/MemberC.java
+++ Controller/MemberC.java
@@ -1,50 +1,33 @@
-package com.lessism.w9;
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE mapper PUBLIC "-//mybatis.org//DTD Mapper 3.0//EN" "http://mybatis.org/dtd/mybatis-3-mapper.dtd">
+<mapper namespace="Member">
 
-import javax.servlet.http.HttpSession;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.lessism.w9.dao.MemberDAO;
-import com.lessism.w9.vo.MemberVO;
-
-@Controller
-@RequestMapping("/member")
-public class MemberC {
+<!-- 회원가입 -->
+	<insert id="join" parameterType="mvo">
+		INSERT INTO
+		w9_member (id, pw, nick, email)
+		VALUES (#{id}, #{pw}, #{nick}, #{email})
+	</insert>
 	
-	@Autowired
-	private MemberDAO mdao = null;
+<!-- 회원탈퇴 -->
+	<delete id="withdrawal">
+		DELETE
+		FROM w9_member
+		WHERE id = #{id}
+	</delete>
 	
-	@RequestMapping(value="/join.w9", method=RequestMethod.GET)
-	public ModelAndView Join() {
-		return new ModelAndView("member/join", "mvo", new MemberVO());
-	}
+<!-- 회원정보수정 -->
+	<update id="infoupd" parameterType="mvo">
+		UPDATE w9_member
+		SET nick = #{nick}, email = #{email}
+		WHERE id = #{id}
+	</update>
 	
-	@RequestMapping(value="/join.w9", method=RequestMethod.POST)
-	public String Join(@ModelAttribute("mvo") MemberVO mvo) {
-		mdao.Join(mvo);
-		return "redirect:/";
-	}
+<!-- 로그인 -->
+	<select id="login" parameterType="mvo" resultType="mvo">
+		SELECT id, nick, email, joindate
+		FROM w9_member
+		WHERE id = #{id} AND pw = #{pw}
+	</select>
 	
-	@RequestMapping(value="/login.w9", method=RequestMethod.GET)
-	public ModelAndView Login() {
-		return new ModelAndView("member/login", "mvo", new MemberVO());
-	}
-	
-	@RequestMapping(value="/login.w9", method=RequestMethod.POST)
-	public String Login(@ModelAttribute("mvo") MemberVO mvo, HttpSession session) {
-		MemberVO rmvo = mdao.Login(mvo);
-		if (rmvo != null) {
-			session.setAttribute("login", true);
-			session.setAttribute("nick", rmvo.getNick());
-			return "redirect:/";
-		} else {
-			return "redirect:/member/login.w9";
-		}
-	}
-	
-}
+</mapper>
