1c62bbbe8f62ce516811adc72d30e1640a1c02f1
Less <46290638+Lessism@users.noreply.github.com>
2019-01-07 22:21:28 +0900

    Update BoardC.java
    Board write

--- Controller/BoardC.java
+++ Controller/BoardC.java
@@ -2,9 +2,15 @@ package com.lessism.w9;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.lessism.w9.dao.BoardDAO;
+import com.lessism.w9.vo.BoardVO;
+import com.lessism.w9.vo.MemberVO;
 
 @Controller
 @RequestMapping("/board")
@@ -18,9 +24,19 @@ public class BoardC {
 	public String Board() {
 		return "/board/board";
 	}
-	@RequestMapping(value="/write.w9")
-	public String Write() {
-		return "/board/write";
+	
+	
+// 게시글 작성
+	@RequestMapping(value="/write.w9", method=RequestMethod.GET)
+	public ModelAndView Write(@SessionAttribute("info") MemberVO mvo, BoardVO bvo) {
+		bvo.setWriter(mvo.getNick());
+		return new ModelAndView("/board/write", "bvo", bvo);
+	}
+	
+	@RequestMapping(value="/write.w9", method=RequestMethod.POST)
+	public String Write(@ModelAttribute("bvo") BoardVO bvo) {
+		bdao.write(bvo);
+		return "redirect:/board/board.w9";
 	}
 
 }
