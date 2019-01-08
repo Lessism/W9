0a0c8573857d9acc40f4a57bbb33df09781a4b56
Less <46290638+Lessism@users.noreply.github.com>
2019-01-08 21:47:58 +0900

    Update MemberC.java
    method naming

--- Controller/MemberC.java
+++ Controller/MemberC.java
@@ -22,31 +22,29 @@ public class MemberC {
 	
 	
 //  회원가입, 회원탈퇴
-	
 	@RequestMapping(value="/join.w9", method=RequestMethod.GET)
-	public ModelAndView Join() {
+	public ModelAndView join() {
 		return new ModelAndView("member/join", "mvo", new MemberVO());
 	}
 	
 	@RequestMapping(value="/join.w9", method=RequestMethod.POST)
 	public String Join(@ModelAttribute("mvo") MemberVO mvo, HttpSession session) {
-		mdao.Join(mvo);
-		mvo = mdao.Login(mvo);
-		session.setAttribute("login", true);
+		mdao.join(mvo);
+		mvo = mdao.login(mvo);
+		session.setAttribute("login", 1);
 		session.setAttribute("info", mvo);
 		return "redirect:/";
 	}
 	
 	@RequestMapping(value="withdrawal.w9")
 	public String Withdrawal(@SessionAttribute("info") MemberVO mvo, HttpSession session) {
-		mdao.Withdrawal(mvo);
+		mdao.delete(mvo);
 		session.invalidate();
 		return "redirect:/";
 	}
 	
 	
 //  회원정보, 회원정보수정
-	
 	@RequestMapping(value="/info.w9", method=RequestMethod.GET)
 	public ModelAndView info(@SessionAttribute("info") MemberVO mvo) {
 		return new ModelAndView("member/info", "mvo", mvo);
@@ -54,14 +52,13 @@ public class MemberC {
 	
 	@RequestMapping(value="/info.w9", method=RequestMethod.POST)
 	public String info(@ModelAttribute MemberVO mvo, HttpSession session) {
-		mdao.InfoUpd(mvo);
+		mdao.update(mvo);
 		session.setAttribute("info", mvo);
 		return "redirect:/";
 	}
 	
 	
 //  로그인, 로그아웃
-	
 	@RequestMapping(value="/login.w9", method=RequestMethod.GET)
 	public ModelAndView Login() {
 		return new ModelAndView("member/login", "mvo", new MemberVO());
@@ -69,9 +66,9 @@ public class MemberC {
 	
 	@RequestMapping(value="/login.w9", method=RequestMethod.POST)
 	public String Login(@ModelAttribute("mvo") MemberVO mvo, HttpSession session) {
-		mvo = mdao.Login(mvo);
+		mvo = mdao.login(mvo);
 		if (mvo != null) {
-			session.setAttribute("login", true);
+			session.setAttribute("login", 1);
 			session.setAttribute("info", mvo);
 			return "redirect:/";
 		} else {
