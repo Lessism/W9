d18e4a5ad018d6a76a751e0d5a9c01a2ec792369
Less <46290638+Lessism@users.noreply.github.com>
2019-01-10 00:34:34 +0900

    Update BoardDAO.java
    Board pagenation

--- DAO/BoardDAO.java
+++ DAO/BoardDAO.java
@@ -19,33 +19,38 @@ public class BoardDAO {
 	private SqlSession db = null;
 	
 	
-//  게시글 작성
+//	게시글 작성
 	public int write(BoardVO bvo) {
 		return db.insert("Board.write", bvo);
 	}
 	
-//  게시글 목록
+//	게시글 목록
 	public List<BoardVO> list() {
 		return db.selectList("Board.list");
 	}
 	
-//  게시글 내용
+//	게시글 내용
 	public BoardVO content(int no) {
 		return db.selectOne("Board.content", no);
 	}
 	
-//  게시글 수정
+//	게시글 수정
 	public int update(BoardVO bvo) {
 		return db.update("Board.update", bvo);
 	}
 	
-//  게시글 삭제
+//	게시글 삭제
 	public int delete(int no) {
 		return db.delete("Board.delete", no);
 	}
 	
-//  이전글, 다음글
+//	이전글, 다음글
 	public String prevnext(Map<String, Object> map) {
 		return db.selectOne("Board.prevnext", map);
+	}
+	
+//	총 게시물
+	public int totalPost() {
+		return db.selectOne("Board.totalpost");
 	}
 }
