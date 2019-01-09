30fe92ddc68c445b8cd96ea9830f57c44c76ba14
Less <46290638+Lessism@users.noreply.github.com>
2019-01-10 00:35:00 +0900

    Update BoardC.java
    Board pagenation

--- Controller/BoardC.java
+++ Controller/BoardC.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,68 +21,70 @@ import com.lessism.w9.vo.MemberVO;
 public class BoardC {
 	
 	@Autowired
-	private BoardDAO bdao = null;
-	
-//  게시글 목록
-	@RequestMapping(value="/list.w9")
-	public ModelAndView list() {
-		return new ModelAndView("/board/list", "list", bdao.list());
-	}
+	private BoardDAO bDAO = null;
 	
 	
-// 게시글 작성
-	@RequestMapping(value="/write.w9", method=RequestMethod.GET)
-	public ModelAndView write(@SessionAttribute("info") MemberVO mvo, BoardVO bvo) {
-		bvo.setWriter(mvo.getNick());
-		return new ModelAndView("/board/write", "bvo", bvo);
-	}
-	
-	@RequestMapping(value="/write.w9", method=RequestMethod.POST)
-	public String write(@ModelAttribute("bvo") BoardVO bvo) {
-		bdao.write(bvo);
-		return "redirect:/board/list.w9";
-	}
-	
-	
-//  게시글 내용
-	@RequestMapping(value="/content.w9", method=RequestMethod.GET)
-	public ModelAndView content(@RequestParam("no") int no, BoardVO bvo) {
-		bvo = bdao.content(no);
-		return new ModelAndView("/board/content", "bvo", bvo);
-	}
-	
-	
-//  게시글 수정
-	@RequestMapping(value="/update.w9", method=RequestMethod.POST)
-	public String update(@ModelAttribute("bvo") BoardVO bvo) {
-		bdao.update(bvo);
-		return "redirect:/board/list.w9";
-	}
-	
-	
-//  게시글 삭제
-	@RequestMapping(value="/delete.w9", method=RequestMethod.GET)
-	public String delete(@RequestParam("no") int no) {
-		bdao.delete(no);
-		return "redirect:/board/list.w9";
-	}
-	
-//  이전글, 다음글
-	@RequestMapping(value={"/prev.w9", "/next.w9"}, method=RequestMethod.GET)
-	public String prevnext(@RequestParam Map<String, Object> map) {
-		if (map.get("prevno") != null) {
-			map.put("minmax", "MAX");
-			map.put("sign", "<");
-			map.put("no", map.get("prevno"));
+//	게시글 목록
+		@RequestMapping(value="/list.w9")
+		public ModelAndView list(Model model) {
+			model.addAttribute("totalpage", (bDAO.totalPost()-1)/10+1);
+			return new ModelAndView("/board/list", "list", bDAO.list());
 		}
-		if (map.get("nextno") != null) {
-			map.put("minmax", "MIN");
-			map.put("sign", ">");
-			map.put("no", map.get("nextno"));
+		
+		
+//	게시글 작성
+		@RequestMapping(value="/write.w9", method=RequestMethod.GET)
+		public ModelAndView write(@SessionAttribute("info") MemberVO mvo, BoardVO bvo) {
+			bvo.setWriter(mvo.getNick());
+			return new ModelAndView("/board/write", "bvo", bvo);
 		}
-		if (bdao.prevnext(map) == null) {
+		
+		@RequestMapping(value="/write.w9", method=RequestMethod.POST)
+		public String write(@ModelAttribute("bvo") BoardVO bvo) {
+			bDAO.write(bvo);
 			return "redirect:/board/list.w9";
 		}
-		return "redirect:/board/content.w9?no="+bdao.prevnext(map);
-	}
+		
+		
+// 	게시글 내용
+		@RequestMapping(value="/content.w9", method=RequestMethod.GET)
+		public ModelAndView content(@RequestParam("no") int no, BoardVO bvo) {
+			bvo = bDAO.content(no);
+			return new ModelAndView("/board/content", "bvo", bvo);
+		}
+		
+		
+//	게시글 수정
+		@RequestMapping(value="/update.w9", method=RequestMethod.POST)
+		public String update(@ModelAttribute("bvo") BoardVO bvo) {
+			bDAO.update(bvo);
+			return "redirect:/board/list.w9";
+		}
+		
+		
+//	게시글 삭제
+		@RequestMapping(value="/delete.w9", method=RequestMethod.GET)
+		public String delete(@RequestParam("no") int no) {
+			bDAO.delete(no);
+			return "redirect:/board/list.w9";
+		}
+		
+//	이전글, 다음글
+		@RequestMapping(value={"/prev.w9", "/next.w9"}, method=RequestMethod.GET)
+		public String prevnext(@RequestParam Map<String, Object> map) {
+			if (map.get("prevno") != null) {
+				map.put("minmax", "MAX");
+				map.put("sign", "<");
+				map.put("no", map.get("prevno"));
+			}
+			if (map.get("nextno") != null) {
+				map.put("minmax", "MIN");
+				map.put("sign", ">");
+				map.put("no", map.get("nextno"));
+			}
+			if (bDAO.prevnext(map) == null) {
+				return "redirect:/board/list.w9";
+			}
+			return "redirect:/board/content.w9?no="+bDAO.prevnext(map);
+		}
 }
