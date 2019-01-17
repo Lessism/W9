9ce6d8fe90ee8f291f9318f0613948edb1cdc629
Less <46290638+Lessism@users.noreply.github.com>
2019-01-17 23:59:56 +0900

    Update BoardDAO.java

--- DAO/BoardDAO.java
+++ DAO/BoardDAO.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lessism.w9.vo.BoardVO;
-
 @Service
 public class BoardDAO {
 	
